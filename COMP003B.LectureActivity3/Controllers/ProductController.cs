2b558f1e9bdf8449e334637fc1a60f2e8af1e17d
samian <sam.iannazzo@yahoo.com>
2026-04-05 13:39:41 -0700

    added controller for product and methods inside

---
+++ COMP003B.LectureActivity3/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using COMP003B.SP25.LectureActivity3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.LectureActivity3.Controllers
+{
+    [Route("product")]
+
+    public class ProductController : Controller
+    {
+        [HttpGet("create")]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost("create")]
+
+        public IActionResult Create([FromForm] ProductController product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+
+            return RedirectToAction("Success", product);
+        }
+
+        [Route("success")]
+
+        public IActionResult Success(ProductController product)
+        {
+            return View(product);
+        }
+    }
+}
